813eade96070a22ac6117d2aabb23cbe45d9e874
edro72018 <159804607+edro72018@users.noreply.github.com>
2024-02-17 01:36:32 -0600

    API inicial

---
+++ crudApi/Controllers/ProductoController.cs
@@ -0,0 +1,17 @@
+/*
+using Microsoft.AspNetCore.Mvc;
+
+namespace crudApi.Controllers
+{
+    [HttpGet]
+    [Route("Producto")]
+    public class ProductoController : ControllerBase{
+        [HttpGet]
+        [Route(create)]
+        public dynamic createProduct(){
+
+        }
+    }
+}
+
+*/
